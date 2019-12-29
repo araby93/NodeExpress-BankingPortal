48aaff56f026aea6647a9cd59e230da0c8974aec
Ahmed Elaraby <araby2305@gmail.com>
2019-12-29 07:41:19 +0200

    Finished module2

--- src/app.js
+++ src/app.js
@@ -9,8 +9,31 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync('./src/json/accounts.json', {encoding: 'UTF8'});
+const accounts = JSON.parse(accountData);
+console.log(accounts);
+
+const userData = fs.readFileSync('./src/json/users.json', {encoding: 'UTF8'});
+const users = JSON.parse(userData);
+
 app.get('/', (request, response) => {
-    response.render('index', {title: 'Index'});
+    response.render('index', {title: 'Account Summary', accounts});
+});
+
+app.get('/savings', (request, response) => {
+    response.render('account', {account: accounts.savings});
+});
+
+app.get('/checking', (request, response) => {
+    response.render('account', {account: accounts.checking});
+});
+
+app.get('/credit', (request, response) => {
+    response.render('account', {account: accounts.credit});
+});
+
+app.get('/profile', (request, response) => {
+    response.render('profile', {user: users[0]});
 });
 
 app.listen(3000, () => {
